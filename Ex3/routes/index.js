912c20a2ffbb7f28fb7daf234d93912252843e1a
Dalin Ourn <ourndalin111@gmail.com>
2022-03-14 01:34:49 +0700

    TP08 vuejs authen.

---
+++ Ex3/routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { login } = require('../services/login');
+const { register } = require('../services/register');
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const result = login(email, password)
+    res.json(result);
+
+})
+
+router.post('/register', function(req, res) {
+    const result = register(req.body);
+    res.json(result);
+})
+
+
+router.post('/ome', (req, res) => {
+    const { email, password } = req.body;
+    const result = login(email, password)
+    res.json(result);
+})
+
+router.get('/serlogin', (req, res) => {
+    const { email, password } = req.body;
+    const result = login(email, password)
+    res.json(result);
+})
+
+module.exports = router;
